3c9ae0836a31e30437b2e7659ed766c62b04e26d
tim12332000 <tim12332000@gmail.com>
2020-04-24 17:16:29 +0800

    first commit

---
+++ Assets/Example.cs
@@ -0,0 +1,49 @@
+﻿using EasyButtons;
+using LitJson;
+using System;
+using System.IO;
+using UnityEngine;
+
+[Serializable]
+public class Test
+{
+	public Vector3 Vec3 = new Vector3(1f, 2f, 3f);
+	public Vector2 Vec2 = new Vector2(4f, 5f);
+}
+
+public class Example : MonoBehaviour
+{
+	[Button]
+	public void DataToJson()
+	{
+		string path = Path.Combine(Application.dataPath, "Test.json");
+		string json = JsonMapper.ToPrettyJson(new Test());
+		File.WriteAllText(path, json);
+	}
+
+	[Button]
+	public void JsonToData()
+	{
+		string path = Path.Combine(Application.dataPath, "Test.json");
+		string json = File.ReadAllText(path);
+		Test test = JsonMapper.ToObject<Test>(json);
+		Debug.LogFormat("vec2{0} vec3 {1}", test.Vec2, test.Vec3);
+	}
+
+	[Button]
+	public void DataToBytes()
+	{
+		string path = Path.Combine(Application.dataPath, "Test.bytes");
+		byte[] b = BytesMapper.ToBytes<Test>(new Test());
+		File.WriteAllBytes(path, b);
+	}
+
+	[Button]
+	public void BytesToData()
+	{
+		string path = Path.Combine(Application.dataPath, "Test.bytes");
+		byte[] b = File.ReadAllBytes(path);
+		Test test = BytesMapper.ToObject<Test>(b);
+		Debug.LogFormat("vec2{0} vec3 {1}", test.Vec2, test.Vec3);
+	}
+}
---
+++ Assets/Plugins/BinaryFormatter/BytesMapper.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public static class BytesMapper
+{
+	private static BinaryFormatter _bf;
+
+	public static BinaryFormatter UnityBinaryFormatter
+	{
+		get
+		{
+			if (_bf == null)
+			{
+				_bf = new BinaryFormatter();
+
+				SurrogateSelector surrogateSelector = new SurrogateSelector();
+				Vector2SerializationSurrogate vector2SS = new Vector2SerializationSurrogate();
+				Vector3SerializationSurrogate vector3SS = new Vector3SerializationSurrogate();
+				surrogateSelector.AddSurrogate(typeof(Vector2), new StreamingContext(StreamingContextStates.All), vector2SS);
+				surrogateSelector.AddSurrogate(typeof(Vector3), new StreamingContext(StreamingContextStates.All), vector3SS);
+				_bf.SurrogateSelector = surrogateSelector;
+			}
+			return _bf;
+		}
+	}
+
+	public static byte[] ToBytes<T>(T obj)
+	{
+		if (obj == null)
+			return null;
+
+		try
+		{
+			using (MemoryStream ms = new MemoryStream())
+			{
+				UnityBinaryFormatter.Serialize(ms, obj);
+				return ms.ToArray();
+			}
+		}
+		catch (Exception e)
+		{
+			Debug.LogErrorFormat("[BytesMapper][ToBytes] get e : {0}", e.Message);
+			return null;
+		}
+	}
+
+	public static T ToObject<T>(byte[] data)
+	{
+		if (data == null)
+			return default(T);
+
+		try
+		{
+			using (MemoryStream ms = new MemoryStream(data))
+			{
+				object obj = UnityBinaryFormatter.Deserialize(ms);
+				return (T)obj;
+			}
+		}
+		catch (Exception e)
+		{
+			Debug.LogErrorFormat("[BytesMapper][ToObject] get e : {0}", e.Message);
+			return default(T);
+		}
+	}
+}
